07318094b08229acdaedd8f35fa02a3f356bb85e
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-13 09:50:51 -0400

    setup page routing & test with console log

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,29 @@
-console.log("JavaScript loaded.");
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Dashboard Page") },
+        { path: "/posts", view: () => console.log("Posts Page") },
+        { path: "/settings", view: () => console.log("Settings Page") }
+    ];
+
+    const checkRoutes = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let currentRoute = checkRoutes.find(checkRoute => checkRoute.isMatch);
+
+    if (!currentRoute) {
+        currentRoute = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    console.log(currentRoute.route.view());
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+});
